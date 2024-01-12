79d8f2c1b09f446d5eea25d7e211e65df0b7efb8
Milad Hasani <milad7212@gmail.com>
2024-01-12 23:08:32 +0330

    add font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+// import "./font.css";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'شرکت پسته حیدری',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir='rtl'>
-      <body className={inter.className}>{children}</body>
+      <body >{children}</body>
     </html>
   )
 }
